9388cc44d46526d2aade92bcab0467412b14a9da
Robert Meli <robertmeli1@outlook.com>
2025-04-17 20:49:42 -0700

    Update page.tsx

--- cvc-api/src/app/page.tsx
+++ cvc-api/src/app/page.tsx
@@ -26,15 +26,13 @@ export default function Home() {
         <div className="flex mt-2 md:mt-8 gap-4 items-center flex-col sm:flex-row">
           <a
           // className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-          className="flex items-center justify-center gap-2 text-sm font-medium sm:text-base"
+          className="flex items-center justify-center inline-block gap-2 text-sm font-medium sm:text-base"
           href={"/docs"}
           target="_blank"
           rel="noopener noreferrer"
         >
           <RainbowButton className="rounded-full inline-block hover:bg-[#383838] h-12">
-            
-                
-                <LuFileSearch className="w-6 h-6"/>
+                {/* <LuFileSearch className="w-6 h-6"/> */}
                 Explore the Docs
             </RainbowButton>
             </a>
